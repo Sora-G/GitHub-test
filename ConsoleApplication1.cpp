c94dffcb7571702403b0fad97618306104e47a0e
そら 鈴木 <jusimatsu0802@gmail.com>
2022-10-19 14:56:09 +0900

    master を追加

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "Hello MasterBranch\n";
+    std::cout << "Hello Master Branch\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
